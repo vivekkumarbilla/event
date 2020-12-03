3785dee9136830523158058cf24e7be3cf70675b
vivekbilla <vivekbilla345@gmail.com>
2020-12-03 13:46:30 +0530

    initial vivek commit

--- event/settings.py
+++ event/settings.py
@@ -138,6 +138,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
